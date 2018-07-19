f49a2b0093241f4d82db2bc92040d5c71490cdd4
E.Yakovlev <eafifa.lover@gmail.com>
2018-07-19 12:26:26 +0300

    toggle group click

--- js/script.js
+++ js/script.js
@@ -77,45 +77,18 @@ $(document).ready(function () {
                 }
             }
         }
-
-        /*
-        for (var i = 0; i < heroes.length; i++) {
-            var myArticle = document.createElement('article');
-            var myH2 = document.createElement('h2');
-            var myPara1 = document.createElement('p');
-            var myPara2 = document.createElement('p');
-            var myPara3 = document.createElement('p');
-            var myList = document.createElement('ul');
-
-            myH2.textContent = heroes[i].name;
-            myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-            myPara2.textContent = 'Age: ' + heroes[i].age;
-            myPara3.textContent = 'Superpowers:';
-
-            var superPowers = heroes[i].powers;
-            for (var j = 0; j < superPowers.length; j++) {
-                var listItem = document.createElement('li');
-                listItem.textContent = superPowers[j];
-                myList.appendChild(listItem);
-            }
-
-            myArticle.appendChild(myH2);
-            myArticle.appendChild(myPara1);
-            myArticle.appendChild(myPara2);
-            myArticle.appendChild(myPara3);
-            myArticle.appendChild(myList);
-
-            section.appendChild(myArticle);
-        }
-        */
     }
 
     $(document).on('click', '.groupSwitcher a', function (e) {
         e.preventDefault();
+        if($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            showMatches(worldCup, "all");
+        }
         $(this).addClass('active');
         $(this).siblings('a').removeClass('active');
         $('.matchWrapper').empty();
-        var group = $(this).html();
+        let group = $(this).html();
         showMatches(worldCup, group);
     });
 });
